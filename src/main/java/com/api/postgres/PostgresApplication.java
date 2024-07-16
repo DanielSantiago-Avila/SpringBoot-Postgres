fc53ea83bc7b07d66b0178959bda78d7b2006419
Daniel <danielajcot@emilianisomascos.edu.gt>
2024-07-15 22:24:14 -0600

    Creacion del Proyecto con spting boot

---
+++ src/main/java/com/api/postgres/PostgresApplication.java
@@ -0,0 +1,13 @@
+package com.api.postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresApplication.class, args);
+	}
+
+}
